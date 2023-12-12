3b6c68adaba877dfe92fab5b92b482928372796f
Emmakjeld <126453240+Emmakjeld@users.noreply.github.com>
2023-12-12 17:04:00 +0100

    kode med ændringer

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+//Burgermenu
 //Trykker på streger - kryds kommer frem
-//document.getElementById("burgermenu").addEventListener("click", vismenu);
+document.getElementById("burgermenu").addEventListener("click", vismenu);
 document.getElementById("burgermenu").onclick = vismenu;
 function vismenu(){
-    alert("vismenu");
+    //alert("vismenu");
     document.getElementById("burgermenu").style.display = "none";
     document.getElementById("kryds").style.display = "flex";
     document.getElementById("menupunkter").style.display = "block";
@@ -10,13 +11,15 @@ function vismenu(){
 
 
 //Trykker på kryds - streger kommer frem
-document.getElementById("kryds").addEventListener("click", offmenu);
+document.getElementById("kryds").addEventListener("click", lukmenu);
 
-function offmenu(){
+function lukmenu(){
     document.getElementById("kryds").style.display = "none";
     document.getElementById("burgermenu").style.display = "flex";
     document.getElementById("menupunkter").style.display = "none";
 }
+
+//Billedekarussel
 //Henter id'er fra HTML
 const glas_01 = document.getElementById("glas_01");
 const glas_02 = document.getElementById("glas_02");
@@ -27,14 +30,18 @@ const venstrePil = document.getElementById("venstre_pil");
 //Arrayet
 const karussel = [glas_01, glas_02, glas_03];
 
-//Et billede bliver vist ad gangen
-karussel[0].style.display = "block";
-karussel[1].style.display = "none";
-karussel[2].style.display = "none";
+if (karussel[0] != null) {
+    
 
-//Knapper
-venstrePil.addEventListener("click", forrigeBillede);
-hoejrePil.addEventListener("click", naesteBillede);
+    //Et billede bliver vist ad gangen
+    karussel[0].style.display = "block";
+    karussel[1].style.display = "none";
+    karussel[2].style.display = "none";
+
+    //Knapper
+    venstrePil.addEventListener("click", forrigeBillede);
+    hoejrePil.addEventListener("click", naesteBillede);
+} //Lukkes her fordi funktionen ikke er blevet kaldt, det bliver den først når man klikker
 
 //Funktioner
 
@@ -52,33 +59,33 @@ function forrigeBillede(){
     karussel[0].style.display = "block";
 }
 
+//Pil til top funktion
+//Henter id'et fra HTML
+const tilTop = document.getElementById("pilTilTop");
 
+//Den laver onscroll på hele vinduet
+window.onscroll = function() {scrollfunction()};
 
-
-
-
-
-//Menupunkterne der bliver vist
-/*function burger(){
-    var links = document.getElementById("menupunkter");
-    if(links.style.display === "block")
-    {
-        links.style.display = "none";
-    } 
-    else {
-        links.style.display = "block";
+//Definerer funktionen --> dette sker når man scroller
+function scrollfunction(){
+    if (document.documentElement.scrollTop > 20){
+        tilTop.style.display = "block";
+    } else {
+        tilTop.style.display = "none"
     }
-}*/
+}
 
-/*
-function menupunkter(){
-    var links = document.getElementById("links");
-    Infinity(links.style.display === "block")
-    {
-        links.style.display = "none";
-    } 
-    else {
-        links.style.display = "block";
-    }
-}*/
+//Klikker på knappen
+tilTop.onclick = function (){topFunction()}
+
+//FOrtæller hvad der skal ske når jeg klikker på knappen
+function topFunction(){
+    document.documentElement.scrollTop = 0
+}
+
+
+
+
+
+
 
